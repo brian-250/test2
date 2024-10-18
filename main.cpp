117bc6634accfe0eb861358b4f75206c62fc3376
Brian Milian <brianmilian@csu.fullerton.edu>
2024-10-18 09:10:17 -0700

    Add return hello function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+int num = 1;
+
+void maybe_return_hello() {
+    if (num == 1) {
+        std::cout << "hello" << std::endl;
+    } else {
+        std::cout << "bye" << std::endl;
+    }
+
+}
+
 int main() {
     std::cout << "Hello World!" << std::endl;
+
+    maybe_return_hello();
     
     return 0;
 }
